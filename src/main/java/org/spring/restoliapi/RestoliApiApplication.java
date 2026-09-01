3467403c23ecbee97e77bd5d675fa2813b5e6f3b
fatratra-png <nyfatratrar@gmail.com>
2026-09-01 18:41:05 +0300

    build: use dotenv-java, load env in main, and db.* properties

--- src/main/java/org/spring/restoliapi/RestoliApiApplication.java
+++ src/main/java/org/spring/restoliapi/RestoliApiApplication.java
@@ -3,10 +3,13 @@ package org.spring.restoliapi;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import io.github.cdimascio.dotenv.Dotenv;
+
 @SpringBootApplication
 public class RestoliApiApplication {
 
     public static void main(String[] args) {
+        Dotenv.configure().ignoreIfMissing().systemProperties().load();
         SpringApplication.run(RestoliApiApplication.class, args);
     }
 
